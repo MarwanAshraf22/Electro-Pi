f536571b9e1d7cb1fce63991ea47acdd5f6ea1b3
Marwan Ashraf <marwanghobashy@gmail.com>
2023-09-09 23:16:03 +0400

    update

---
+++ National ID Card Recognition/app.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np
+import pytesseract
+import matplotlib.pyplot as plt
+import pandas as pd
+import PIL
+from pytesseract import Output
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
