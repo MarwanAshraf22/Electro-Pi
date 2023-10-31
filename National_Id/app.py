32941ca096309859ea939c9d06fe1c17d356394c
Marwan Ashraf <marwanghobashy@gmail.com>
2023-10-31 22:33:37 +0400

    update

--- National_Id/app.py
+++ National_Id/app.py
@@ -1,76 +1,33 @@
-import cv2
-import numpy as np
+import streamlit as st
+from PIL import Image
 import pytesseract
 import pandas as pd
-import streamlit as st
-from pytesseract import Output
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-
-def display_sidebar():
-    with st.sidebar:
-        st.title("National ID Card Recognition")
-        st.info("This project is powered by electropi.ai using Tesseract OCR and cv2 to detect text on ID cards")
-
-def extract_information(df_text):
-    names = []
-    ages = []
-    id_numbers = []
-
-    for idx, row in df_text.iterrows():
-        text = row['text'].strip()
-
-        if text.startswith('Name:'):
-            names.append(text[5:].strip())
-        elif text.startswith('Age:'):
-            ages.append(text[4:].strip())
-        elif text.startswith('ID:'):
-            id_numbers.append(text[3:].strip())
-
-    info_df = pd.DataFrame({'Name': names, 'Age': ages, 'ID Number': id_numbers})
-    return info_df
-
-def upload_id_card(file):
-    try:
-        st.title("Upload Your ID card please in jpg or png format!")
-
-        if file is not None:
-            st.success("File uploaded successfully!")
-            st.image(file, 'this is your uploaded id')
-
-            img_bytes = np.frombuffer(file.read(), np.uint8)
-            img = cv2.imdecode(img_bytes, cv2.IMREAD_COLOR)
-            height, width, _ = img.shape
-            my_config = r'--psm 11 --oem 3'
-
-            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            df = pytesseract.image_to_data(gray, output_type=Output.DATAFRAME, config=my_config)
-            df.dropna(inplace=True)
-            df_text = pd.DataFrame(df['text'])
-
-            boxes = pytesseract.image_to_boxes(img, config=my_config)
-            for box in boxes.splitlines():
-                box = box.split(' ')
-                img_text = cv2.rectangle(img, (int(box[1]), height - int(box[2])),
-                                         (int(box[3]), height - int(box[4])), (0, 255, 0), 2)
-
-            st.image(img_text, 'This is detected text')
-
-            info_df = extract_information(df_text)
-
-            st.write("Extracted Information:")
-            st.dataframe(info_df)
-
-        else:
-            st.info("Please upload a file in jpg or png format.")
-
-    except Exception as e:
-        st.error(f'Error: {e}')
+def perform_ocr(image):
+    text = pytesseract.image_to_string(image)
+    lines = text.split('\n')
+    non_empty_lines = [line for line in lines if line.strip()]
+    data = {'Text': non_empty_lines}
+    df = pd.DataFrame(data)
+    return df.iloc[2:7]  # Select rows 2 to 5 (0-based indexing)
 
 def main():
-    display_sidebar()
-    file = st.file_uploader("Upload Your Dataset", type=['jpg', 'png'])
-    upload_id_card(file)
+    st.title("Simple OCR Web App")
 
-if __name__ == '__main__':
+    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png"])
+
+    if uploaded_file is not None:
+        st.image(uploaded_file, caption='Uploaded Image.', use_column_width=True)
+        st.write("")
+        st.write("Classifying...")
+
+        # Perform OCR
+        image = Image.open(uploaded_file)
+        df_text = perform_ocr(image)
+
+        # Display the extracted text as DataFrame
+        st.header("Extracted Text")
+        st.dataframe(df_text)
+
+if __name__ == "__main__":
     main()
